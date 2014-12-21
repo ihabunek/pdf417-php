2e029eae92799415cffeae6083fd5b08941daf4b
Ivan Habunek <ivan.habunek@gmail.com>
2014-12-21 16:00:59 +0100

    Started tests

---
+++ tests/Renderers/JsonRendererTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BigFish\PDF417\Tests\Renderers;
+
+use BigFish\PDF417\BarcodeData;
+use BigFish\PDF417\Renderers\JsonRenderer;
+
+class JsonRendererTest extends \PHPUnit_Framework_TestCase
+{
+    public function testContentType()
+    {
+        $renderer = new JsonRenderer();
+        $actual = $renderer->getContentType();
+        $expected = "application/json";
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testRender()
+    {
+        $data = new BarcodeData();
+        $data->codes = [
+            [true, false],
+            [false, true],
+        ];
+
+        $renderer = new JsonRenderer();
+        $actual = $renderer->render($data);
+        $expected = "[[1,0],[0,1]]";
+
+        $this->assertSame($expected, $actual);
+    }
+}
