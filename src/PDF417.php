ihabunek/pdf417-php

commit c601b113a0230a515565a7b59b499c31ba6db90e

Test for Codes class, removed an unnecessary method.

src/PDF417.php
@@ -72,6 +72,7 @@ class PDF417
         // Iterate over rows
         $codes = [];
         foreach ($grid as $rowNum => $row) {
+            $table = $row % 3;
             $rowCodes = [];
 
             // Add starting code word
@@ -79,16 +80,16 @@ class PDF417
 
             // Add left-side code word
             $left = $this->getLeftCodeWord($rowNum, $rows, $columns, $secLev);
-            $rowCodes[] = Codes::getCodeForRow($rowNum, $left);
+            $rowCodes[] = Codes::getCode($table, $left);
 
             // Add data code words
             foreach ($row as $word) {
-                $rowCodes[] = Codes::getCodeForRow($rowNum, $word);
+                $rowCodes[] = Codes::getCode($table, $word);
             }
 
             // Add right-side code word
             $right = $this->getRightCodeWord($rowNum, $rows, $columns, $secLev);
-            $rowCodes[] = Codes::getCodeForRow($rowNum, $right);
+            $rowCodes[] = Codes::getCode($table, $right);
 
             // Add ending code word
             $rowCodes[] = self::STOP_CHARACTER;

src/Codes.php
@@ -2,6 +2,10 @@
 
 namespace BigFish\PDF417;
 
+/**
+ * Converts high-level (base 929) code words into low-level code words (binary
+ * patterns for drawing the bar code).
+ */
 class Codes
 {
     private static $codes = [
@@ -412,6 +416,14 @@ class Codes
         ]
     ];
 
+    /**
+     * Returns a low-level code work corresponding to the given high-level code
+     * word in the given table.
+     *
+     * @param  integer $table The table to look in (0-2).
+     * @param  integer $word  The code word to encode (0-928).
+     * @return integer        The encoded code word.
+     */
     public static function getCode($table, $word)
     {
         if (!isset(self::$codes[$table][$word])) {
@@ -419,11 +431,5 @@ class Codes
         }
 
         return self::$codes[$table][$word];
-    }
-
-    public static function getCodeForRow($row, $word)
-    {
-        $table = $row % 3;
-        return self::getCode($table, $word);
     }
 }

tests/CodesTest.php
@@ -1,0 +1,40 @@
+<?php
+
+namespace BigFish\PDF417\Tests;
+
+use BigFish\PDF417\Codes;
+
+class CodesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetCode()
+    {
+        $this->assertSame(0x1d5c0, Codes::getCode(0, 0));
+        $this->assertSame(0x1f560, Codes::getCode(1, 0));
+        $this->assertSame(0x1abe0, Codes::getCode(2, 0));
+
+        $this->assertSame(0x1bef4, Codes::getCode(0, 928));
+        $this->assertSame(0x13f26, Codes::getCode(1, 928));
+        $this->assertSame(0x1c7ea, Codes::getCode(2, 928));
+
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Invalid code word [0][929]
+     *
+     * @return [type] [description]
+     */
+    public function testInvalidCode()
+    {
+        $this->assertSame(0x1abe0, Codes::getCode(0, 929));
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Invalid code word [3][0]
+     */
+    public function testInvalidTable()
+    {
+        $this->assertSame(0x1abe0, Codes::getCode(3, 0));
+    }
+}

commit 50d79d0236d336d665a59767dc3d0869ab0594af

Fixed a bug introduced in commit c601b11

src/PDF417.php
@@ -72,7 +72,7 @@ class PDF417
         // Iterate over rows
         $codes = [];
         foreach ($grid as $rowNum => $row) {
-            $table = $row % 3;
+            $table = $rowNum % 3;
             $rowCodes = [];
 
             // Add starting code word